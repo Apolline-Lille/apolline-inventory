5217c26fbfa42bdee7b1f2528ac662168c58e3e1
Christophe Ribeiro <christo.ribeiro@gmail.com>
2015-02-11 11:51:37 +0100

    feature login

--- app/controllers/UserManager.scala
+++ app/controllers/UserManager.scala
@@ -6,31 +6,65 @@ import reactivemongo.api._
 import play.modules.reactivemongo.MongoController
 import play.modules.reactivemongo.json.collection.JSONCollection
 
+import play.api.libs.json._
 import play.api.data._
 import play.api.data.Forms._
+import scala.concurrent._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 object UserManager extends Controller with MongoController {
 
-  case class LoginInfo(login: String, passwd : String)  
-  
+  def usersCollection = db.collection[JSONCollection]("users")
+
+  case class LoginInfo(login: String, passwd : String)
+
      private val loginForm = Form[LoginInfo](
         mapping(
             "login" -> nonEmptyText,
             "passwd" -> nonEmptyText)(LoginInfo.apply)(LoginInfo.unapply)
         )
-  
+
   def welcome = Action { request =>
     request.session.get("connected").map { user => Ok("Hello " + user)
     }.getOrElse { Ok(views.html.index("Redirigé car pas connecté")).withSession("connected" -> "Coco l'asticot") }
   }
-  
+
   def loginPage = Action {
     Ok(views.html.login(loginForm))
   }
-  
-  def userWelcome = Action { request =>
-    val inputUser
-    
-    
+
+  def loginSubmit = Action.async { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {
+        Future.successful(BadRequest(views.html.login(formWithErrors)))
+      },
+      loginData => {
+        val login = loginData.login
+        val passwd = loginData.passwd
+
+        val cursor = usersCollection.
+        find(Json.obj("username" -> login, "password" -> passwd)).
+        cursor[JsObject].
+        collect[List]()
+
+        // val newUser = models.User(userData.name, userData.age)
+        // val id = models.User.create(newUser)
+        // Redirect(routes.Application.home(id))
+        cursor.map { result =>
+          if(result.isEmpty) {
+            Ok(views.html.login(loginForm.withError("login", "login/password incorrect")))
+          } else {
+            Ok("logged in").withSession("user" -> login)
+          }
+        }
+      }
+    )
   }
+
+  def userWelcome = Action { request =>
+    // val inputUser
+    Ok
+  }
+
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,9 +1,11 @@
 package models
 
 import reactivemongo.api._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
-case class User(username : String,
-                password: String)
+case class User(username : String, password: String)
 
 object User {
+    implicit val userFormat = Json.format[User]
 }
