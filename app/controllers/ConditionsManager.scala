f7fd0ced9f8f535d6d391cea9d1156a75fca6870
Thomas <thomas@spirals-Precision-M4400.(none)>
2015-06-10 11:02:53 +0200

    feat(conditions):Add routes for add general informations and select module
    Add routes :
    	- /campaigns/campaign/:id/form for display form for add general informations
    	- /campaigns/campaign/:id/form/module for display list module for select one

--- app/controllers/ConditionsManager.scala
+++ app/controllers/ConditionsManager.scala
@@ -1,11 +1,13 @@
 package controllers
 
 import java.lang.annotation.Annotation
+import java.util.Date
 
 import com.wordnik.swagger.annotations._
 import models._
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.data.validation.Constraints._
 import play.api.i18n.Messages
 import play.api.libs.json.{JsObject, Json}
 import play.api.mvc._
@@ -14,6 +16,8 @@ import reactivemongo.extensions.BSONFormats.BSONObjectIDFormat
 import scala.concurrent._
 
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+case class ConditionForm(debut:String,fin:Option[String],commentaire:Option[String])
 
 /**
  * This object is a controller for manage all condition
@@ -27,13 +31,23 @@ trait ConditionsManagerLike extends Controller{
    */
   val conditionsDao:ConditionDao=ConditionDaoObj
 
-  val campaignDao:CampagneDao=CampagneDaoObj
+  val moduleManager:ModuleManagerLike=ModuleManager
+
+  val campaignManager:CampagneManagerLike=CampagneManager
+
+  val form=Form[ConditionForm](
+    mapping(
+      "debut"->nonEmptyText.verifying(pattern("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}".r)),
+      "fin"->optional(text.verifying(pattern("\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}".r))),
+      "commentaire"->optional(text)
+    )(ConditionForm.apply)(ConditionForm.unapply)
+  )
 
   /****************** Route methods ***********/
 
   /**
-   * This method is call when the user is on the page /campaigns/conditions. It list condtions for a campaign
-   * @return Return Ok Action when the user is on the page /campaigns/conditions with the list of conditions for a campaign
+   * This method is call when the user is on the page /campaigns/campaign/:id. It list condtions for a campaign
+   * @return Return Ok Action when the user is on the page /campaigns/campaign/:id with the list of conditions for a campaign
    *         Return Redirect Action when the user is not log in
    *         Return Internal Server Error Action when have mongoDB error
    */
@@ -53,14 +67,105 @@ trait ConditionsManagerLike extends Controller{
     implicit request =>
       //Verify if user is connect
       UserManager.doIfconnectAsync(request) {
-        campaignDao.findById(BSONObjectID(id)).map(
-          data=> data match{
-            case None=>Redirect(routes.CampagneManager.listCampaign())
-            case Some(campaign)=>Ok(views.html.campaign.listCondition(campaign))
-          }
-        )
+
+        //Verify if campaign exist
+        campaignManager.doIfCampaignFound(BSONObjectID(id)){
+
+          //If campaign found
+          campaign=>future{Ok(views.html.campaign.listCondition(campaign))}
+        }{
+
+          //If campaign not found
+          _ => future{Redirect(routes.CampagneManager.listCampaign())}
+        }
       }
   }
+
+  /**
+   * This method is call when the user is on the page /campaigns/campaign/:id/form. It display form for insert condition general information
+   * @return Return Ok Action when the user is on the page /campaigns/campaign/:id/form with a form for insert condition general information
+   *         Return Redirect Action when the user is not log in
+   *         Return Internal Server Error Action when have mongoDB error
+   */
+  @ApiOperation(
+    nickname = "conditions/form",
+    value = "Get the html page with form for insert condition general information",
+    notes = "Get the html page with form for insert condition general information",
+    httpMethod = "GET")
+  @ApiResponses(Array(
+    new ApiResponse(code=303,message="Move resource to the login page at /login if the user is not log"),
+    new ApiResponse(code=500,message="Have a mongoDB error")
+  ))
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name="id",value="Campaign id",required=true,dataType="String",paramType="path")
+  ))
+  def formGeneral(id:String)=Action.async{
+    implicit request =>
+      //Verify if user is connect
+      UserManager.doIfconnectAsync(request) {
+
+        //Verify if campaign exist
+        campaignManager.doIfCampaignFound(BSONObjectID(id)){
+
+          //If campaign found
+          campaign=>future{Ok(views.html.campaign.formConditions(form,printStateForm("infoCondition",campaign.types,id)))}
+        }{
+
+          //If campaign not found
+          _ => future{Redirect(routes.CampagneManager.listCampaign())}
+        }
+      }
+  }
+
+  /**
+   * This method is call when the user is on the page /campaigns/campaign/:id/form/module. It display the module list for associat one to a condition
+   * @return Return Ok Action when the user is on the page /campaigns/campaign/:id/form/module with the module list for associat one to a condition
+   *         Return Redirect Action when the user is not log in
+   *         Return Internal Server Error Action when have mongoDB error
+   */
+  @ApiOperation(
+    nickname = "conditions/form/module",
+    value = "Get the html page with the module list for associat one to a condition",
+    notes = "Get the html page with the module list for associat one to a condition",
+    httpMethod = "GET")
+  @ApiResponses(Array(
+    new ApiResponse(code=303,message="Move resource to the login page at /login if the user is not log"),
+    new ApiResponse(code=500,message="Have a mongoDB error")
+  ))
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(name="id",value="Campaign id",required=true,dataType="String",paramType="path"),
+    new ApiImplicitParam(name="filtre",value="Module type for filter module",required=true,dataType="String",paramType="query")
+  ))
+  def formModule(id:String,filtre:String)=Action.async{
+    implicit request=>
+      //Verify if user is connect
+      UserManager.doIfconnectAsync(request) {
+
+        //Verify if campaign exist
+        campaignManager.doIfCampaignFound(BSONObjectID(id)){
+
+          //If campaign found
+          campaign=>
+
+            //Display module inventary
+            moduleManager.getInventaryModule(filtre) {
+              (modules, listType) => Ok(views.html.campaign.listModule(modules, listType, filtre,id, printStateForm("module",campaign.types, id)))
+            }
+        }{
+          //If campaign not found
+          _ => future{Redirect(routes.CampagneManager.listCampaign())}
+        }
+      }
+  }
+
+  /**
+   * Display html code for navigate in the form for create a new conditions
+   * @param activ Name of the link activ
+   * @param types Condition type
+   * @param id Id of the campaign
+   * @return Html code for navigate in the form for create a new conditions
+   */
+  def printStateForm(activ:String,types:String,id:String)=views.html.campaign.stateForm(activ,types,id)
 }
 
 @Api(value = "/conditions", description = "Operations for conditions")
--- test/ConditionsManagerSpec.scala
+++ test/ConditionsManagerSpec.scala
@@ -1,13 +1,16 @@
+import java.util.Date
+
 import controllers._
-import models.{Campagne, CampagneDao, ConditionDao}
+import models.{Module, Campagne, CampagneDao, ConditionDao}
 import org.junit.runner.RunWith
 import org.specs2.matcher.{MatchResult, Expectable, Matcher}
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
+import play.api.mvc.Result
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, WithApplication}
-import reactivemongo.bson.BSONObjectID
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent._
@@ -25,6 +28,8 @@ class ConditionsManagerSpec extends Specification with Mockito {
     override def apply[S <: String](t: Expectable[S]): MatchResult[S] = result(a.r.findAllMatchIn(t.value).size == b, "okMessage", "not found " + b + " times but " + a.r.findAllMatchIn(t.value).size + " times " + a, t)
   }
 
+  val date=new Date(115,3,22)
+
   val bson = BSONObjectID.generate
   val bson2 = BSONObjectID.generate
   val bson3 = BSONObjectID.generate
@@ -32,16 +37,40 @@ class ConditionsManagerSpec extends Specification with Mockito {
 
   def fixture=new {
     val conditionDaoMock=mock[ConditionDao]
-    val campaignDaoMock=mock[CampagneDao]
+    val campaignManagerMock=mock[CampagneManagerLike]
+    val moduleManagerMock=mock[ModuleManagerLike]
     val controller=new ConditionsManagerTest{
       override val conditionsDao=conditionDaoMock
-      override val campaignDao=campaignDaoMock
+      override val campaignManager=campaignManagerMock
+      override val moduleManager=moduleManagerMock
     }
   }
 
   "When user is not connected, ConditionsManager" should {
     "redirect to login for resource /campaigns/campaign/:id" in new WithApplication {
       route(FakeRequest(GET, "/campaigns/campaign/"+bson.stringify)).map(
+        r => {
+          status(r) must equalTo(SEE_OTHER)
+          header("Location", r) must equalTo(Some("/login"))
+        }
+      ).getOrElse(
+          failure("Pas de retour de la fonction")
+        )
+    }
+
+    "redirect to login for resource /campaigns/campaign/:id/form" in new WithApplication {
+      route(FakeRequest(GET, "/campaigns/campaign/"+bson.stringify+"/form")).map(
+        r => {
+          status(r) must equalTo(SEE_OTHER)
+          header("Location", r) must equalTo(Some("/login"))
+        }
+      ).getOrElse(
+          failure("Pas de retour de la fonction")
+        )
+    }
+
+    "redirect to login for resource /campaigns/campaign/:id/form/module" in new WithApplication {
+      route(FakeRequest(GET, "/campaigns/campaign/"+bson.stringify+"/form/module")).map(
         r => {
           status(r) must equalTo(SEE_OTHER)
           header("Location", r) must equalTo(Some("/login"))
@@ -57,7 +86,9 @@ class ConditionsManagerSpec extends Specification with Mockito {
       val f=fixture
       val camp=Campagne(bson,"camp","type",List())
 
-      f.campaignDaoMock.findById(org.mockito.Matchers.eq(bson))(any[ExecutionContext]) returns future{Some(camp)}
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,p:(Campagne=>Future[Result]),_) => p.apply(camp)
+      }}
 
       val r=f.controller.listConditions(bson.stringify).apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify).withSession("user" -> """{"login":"test"}"""))
 
@@ -67,20 +98,203 @@ class ConditionsManagerSpec extends Specification with Mockito {
       content must contain("<div class=\"row\"><span class=\"bold\">Type</span> : type</div>")
       content must contain("<div class=\"row\"><span class=\"bold\">Conditions</span> : 0</div>")
 
-      there was one(f.campaignDaoMock).findById(org.mockito.Matchers.eq(bson))(any[ExecutionContext])
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
     }
 
     "send redirect if campaign not found" in new WithApplication {
       val f=fixture
 
-      f.campaignDaoMock.findById(org.mockito.Matchers.eq(bson))(any[ExecutionContext]) returns future{None}
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,_,p:(Unit=>Future[Result])) => p.apply()
+      }}
 
       val r=f.controller.listConditions(bson.stringify).apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify).withSession("user" -> """{"login":"test"}"""))
 
       status(r) must equalTo(SEE_OTHER)
       header("Location",r) must beSome("/campaigns")
 
-      there was one(f.campaignDaoMock).findById(org.mockito.Matchers.eq(bson))(any[ExecutionContext])
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+    }
+  }
+
+  "When user is on resource /campaigns/campaign/:id/form, ConditionsManager" should{
+    "send 200 Ok page with an empty form" in new WithApplication {
+      val f=fixture
+      val camp=Campagne(bson,"camp","type",List())
+
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,p:(Campagne=>Future[Result]),_) => p.apply(camp)
+      }}
+
+      val r=f.controller.formGeneral(bson.stringify).apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify+"/form").withSession("user" -> """{"login":"test"}"""))
+
+      status(r) must equalTo(OK)
+      val content=contentAsString(r)
+      content must contain("<input type=\"text\" id=\"debut\" name=\"debut\" value=\"\" class=\"form-control\"/>")
+      content must contain("<input type=\"text\" id=\"fin\" name=\"fin\" value=\"\" class=\"form-control\"/>")
+      content must contain("<textarea id=\"commentaire\" name=\"commentaire\" class=\"form-control\"></textarea>")
+
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+    }
+
+    "send redirect if campaign not found" in new WithApplication {
+      val f=fixture
+
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,_,p:(Unit=>Future[Result])) => p.apply()
+      }}
+
+      val r=f.controller.formGeneral(bson.stringify).apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify+"/form").withSession("user" -> """{"login":"test"}"""))
+
+      status(r) must equalTo(SEE_OTHER)
+      header("Location",r) must beSome("/campaigns")
+
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+    }
+  }
+
+  "When user is on resource /campaigns/campaign/:id/form/module, ConditionsManager" should{
+    "send 200 Ok page with message 'Aucun résultat trouvé'" in new WithApplication {
+      val f=fixture
+      val camp=Campagne(bson,"camp","type",List())
+
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,p:(Campagne=>Future[Result]),_) => p.apply(camp)
+      }}
+
+      f.moduleManagerMock.getInventaryModule(org.mockito.Matchers.eq(""))(any[(List[Module],List[BSONDocument])=>Result]) answers {(params,_)=>params match{
+        case Array(_,p:((List[Module],List[BSONDocument])=>Result)) => future{p.apply(List(),List())}
+      }}
+
+      val r=f.controller.formModule(bson.stringify,"").apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify+"/form/module").withSession("user" -> """{"login":"test"}"""))
+
+      status(r) must equalTo(OK)
+      contentAsString(r) must contain("<h3 style=\"text-align:center\">Aucun résultat trouvé</h3>")
+
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+      there was one(f.moduleManagerMock).getInventaryModule(org.mockito.Matchers.eq(""))(any[(List[Module],List[BSONDocument])=>Result])
+    }
+
+    "send 200 Ok page with 1 result" in new WithApplication {
+      val f=fixture
+      val camp=Campagne(bson,"camp","type",List())
+      val modules=List(
+        Module(bson,"idMod","typesMod",date,List(bson),List(bson),Some("un com"))
+      )
+
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,p:(Campagne=>Future[Result]),_) => p.apply(camp)
+      }}
+
+      f.moduleManagerMock.getInventaryModule(org.mockito.Matchers.eq(""))(any[(List[Module],List[BSONDocument])=>Result]) answers {(params,_)=>params match{
+        case Array(_,p:((List[Module],List[BSONDocument])=>Result)) => future{p.apply(modules,List())}
+      }}
+
+      val r=f.controller.formModule(bson.stringify,"").apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify+"/form/module").withSession("user" -> """{"login":"test"}"""))
+
+      status(r) must equalTo(OK)
+      val content = contentAsString(r)
+      content must contain("typesMod")
+      content must contain("idMod")
+      content must contain("<div class=\"row\"><span class=\"bold\">Commentaires</span> : un com</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Date d&#x27;assemblage</span> : 22/04/2015</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Cartes</span> : 1</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Capteurs</span> : 1</div>")
+
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+      there was one(f.moduleManagerMock).getInventaryModule(org.mockito.Matchers.eq(""))(any[(List[Module],List[BSONDocument])=>Result])
+    }
+
+    "send 200 Ok page with 2 result" in new WithApplication {
+      val f=fixture
+      val camp=Campagne(bson,"camp","type",List())
+      val modules=List(
+        Module(bson,"idMod","typesMod",date,List(bson),List(bson),Some("un com")),
+        Module(bson,"idMod2","typesMod2",date,List(bson,bson2),List(bson,bson2,bson3),Some("un com2"))
+      )
+
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,p:(Campagne=>Future[Result]),_) => p.apply(camp)
+      }}
+
+      f.moduleManagerMock.getInventaryModule(org.mockito.Matchers.eq(""))(any[(List[Module],List[BSONDocument])=>Result]) answers {(params,_)=>params match{
+        case Array(_,p:((List[Module],List[BSONDocument])=>Result)) => future{p.apply(modules,List())}
+      }}
+
+      val r=f.controller.formModule(bson.stringify,"").apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify+"/form/module").withSession("user" -> """{"login":"test"}"""))
+
+      status(r) must equalTo(OK)
+      val content = contentAsString(r)
+      content must contain("typesMod")
+      content must contain("idMod")
+      content must contain("<div class=\"row\"><span class=\"bold\">Commentaires</span> : un com</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Date d&#x27;assemblage</span> : 22/04/2015</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Cartes</span> : 1</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Capteurs</span> : 1</div>")
+      content must contain("typesMod2")
+      content must contain("idMod2")
+      content must contain("<div class=\"row\"><span class=\"bold\">Commentaires</span> : un com2</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Cartes</span> : 2</div>")
+      content must contain("<div class=\"row\"><span class=\"bold\">Capteurs</span> : 3</div>")
+
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+      there was one(f.moduleManagerMock).getInventaryModule(org.mockito.Matchers.eq(""))(any[(List[Module],List[BSONDocument])=>Result])
+    }
+
+    "send redirect if campaign not found" in new WithApplication {
+      val f=fixture
+
+      f.campaignManagerMock.doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]]) answers {(params,_) => params match{
+        case Array(_,_,p:(Unit=>Future[Result])) => p.apply()
+      }}
+
+      val r=f.controller.formModule(bson.stringify,"").apply(FakeRequest(GET,"/campaigns/campaign/"+bson.stringify+"/form/module").withSession("user" -> """{"login":"test"}"""))
+
+      status(r) must equalTo(SEE_OTHER)
+      header("Location",r) must beSome("/campaigns")
+
+      there was one(f.campaignManagerMock).doIfCampaignFound(org.mockito.Matchers.eq(bson))(any[Campagne=>Future[Result]])(any[Unit=>Future[Result]])
+    }
+  }
+
+  "When method printStateForm is called, ConditionsManager" should{
+    "display 3 links with 'general information', 'modules' and 'validate' for Test conditions" in new WithApplication{
+      val f=fixture
+
+      val r=f.controller.printStateForm("infoConditions","Test",bson.stringify)
+
+      val content=contentAsString(r)
+      content must contain("Informations générales")
+      content must contain("Module")
+      content must contain("Validation")
+      content must not contain("Paramétre")
+      content must not contain("Localisation")
+    }
+
+    "display 3 links with 'general information', 'modules', 'parameter' and 'validate' for Calibrations conditions" in new WithApplication{
+      val f=fixture
+
+      val r=f.controller.printStateForm("infoConditions","Calibration",bson.stringify)
+
+      val content=contentAsString(r)
+      content must contain("Informations générales")
+      content must contain("Module")
+      content must contain("Validation")
+      content must contain("Paramétres")
+      content must not contain("Localisation")
+    }
+
+    "display 3 links with 'general information', 'modules', 'localisation' and 'validate' for Ground conditions" in new WithApplication{
+      val f=fixture
+
+      val r=f.controller.printStateForm("infoConditions","Terrain",bson.stringify)
+
+      val content=contentAsString(r)
+      content must contain("Informations générales")
+      content must contain("Module")
+      content must contain("Validation")
+      content must not contain("Paramétre")
+      content must contain("Localisation")
     }
   }
 }
