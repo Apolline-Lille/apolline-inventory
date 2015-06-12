8da3cb3441171e3e8ab261c86920b20a3e7028e4
Thomas <thomas@spirals-Precision-M4400.(none)>
2015-06-12 13:57:48 +0200

    feat(parameter):Add parameter section in campaign
    Add new section parameter in campaign
    This section is used to manage calibration parameter

---
+++ app/controllers/ParametreManager.scala
@@ -0,0 +1,55 @@
+package controllers
+
+import com.wordnik.swagger.annotations._
+import models._
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.Messages
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc._
+import reactivemongo.bson.BSONObjectID
+import reactivemongo.extensions.BSONFormats.BSONObjectIDFormat
+import scala.concurrent._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+/**
+ * This object is a controller for manage all parameter
+ */
+trait ParametreManagerLike extends Controller{
+
+  /************* Property *********************/
+
+  /**
+   * DAO for parameters
+   */
+  val parameterDao:ParametresDao=ParametresDaoObj
+
+  /****************** Route methods ***********/
+
+  /**
+   * This method is call when the user is on the page /campaigns/parameter. It list calibration parameter
+   * @return Return Ok Action when the user is on the page /campaigns/parameter with the list of calibration parameter
+   *         Return Redirect Action when the user is not log in
+   *         Return Internal Server Error Action when have mongoDB error
+   */
+  @ApiOperation(
+    nickname = "parameter",
+    value = "Get the html page for list calibration parameter",
+    notes = "Get the html page for list calibration parameter",
+    httpMethod = "GET")
+  @ApiResponses(Array(
+    new ApiResponse(code=303,message="Move resource to the login page at /login if the user is not log"),
+    new ApiResponse(code=500,message="Have a mongoDB error")
+  ))
+  def listParameter()=Action.async{
+    implicit request =>
+      //Verify if user is connect
+      UserManager.doIfconnectAsync(request) {
+        future{Ok(views.html.param.listParam())}
+      }
+  }
+}
+
+@Api(value = "/param", description = "Operations for calibration parameter")
+object ParametreManager extends ParametreManagerLike
---
+++ test/ParametreManagerSpec.scala
@@ -0,0 +1,54 @@
+import controllers._
+import models._
+import org.junit.runner.RunWith
+import org.specs2.matcher.{MatchResult, Expectable, Matcher}
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import play.api.libs.json.{JsObject, Writes, Json}
+import play.api.mvc.{Results, Action, Result}
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, WithApplication}
+import reactivemongo.bson.BSONObjectID
+import reactivemongo.extensions.BSONFormats.BSONObjectIDFormat
+import reactivemongo.core.commands.{LastError, GetLastError}
+
+import scala.concurrent._
+import scala.concurrent.duration.Duration
+
+@RunWith(classOf[JUnitRunner])
+class ParametreManagerSpec extends Specification with Mockito {
+
+  class ParametreManagerTest extends ParametreManagerLike
+
+  case class matchRegex(a: String) extends Matcher[String]() {
+    override def apply[S <: String](t: Expectable[S]): MatchResult[S] = result(a.r.findFirstIn(t.value).nonEmpty, "okMessage", t.value + "not found " + a, t)
+  }
+
+  case class contains(a: String, b: Int) extends Matcher[String]() {
+    override def apply[S <: String](t: Expectable[S]): MatchResult[S] = result(a.r.findAllMatchIn(t.value).size == b, "okMessage", "not found " + b + " times but " + a.r.findAllMatchIn(t.value).size + " times " + a, t)
+  }
+
+  val bson = BSONObjectID.generate
+  val bson2 = BSONObjectID.generate
+
+  def fixture = new {
+    val parameterDaoMock=mock[ParametresDao]
+    val controller=new ParametreManagerTest{
+      override val parameterDao:ParametresDao=parameterDaoMock
+    }
+  }
+
+  "When user is not connected, ParametreManager" should {
+    "redirect to login for resource /campaigns/parameter" in new WithApplication {
+      route(FakeRequest(GET, "/campaigns/parameter")).map(
+        r => {
+          status(r) must equalTo(SEE_OTHER)
+          header("Location", r) must equalTo(Some("/login"))
+        }
+      ).getOrElse(
+          failure("Pas de retour de la fonction")
+        )
+    }
+  }
+}
