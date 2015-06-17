a1c326f24ce174c08800b7558134ca6c4a127992
Thomas <thomas@spirals-Precision-M4400.(none)>
2015-06-17 13:53:19 +0200

    feat(condition): Remove subclass of Condition
    Remove condition subclass Test, Calibration and Terrain
    Change abstract class Condition to a case class

--- app/models/Condition.scala
+++ app/models/Condition.scala
@@ -11,52 +11,18 @@ import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.parsing.json.JSONObject
 
-abstract class Condition(_id:BSONObjectID,dateDebut:Date,dateFin:Option[Date],commentaire:Option[String],modules:BSONObjectID)
-case class Test(_id:BSONObjectID=BSONObjectID.generate,dateDebut:Date,dateFin:Option[Date],commentaire:Option[String],modules:BSONObjectID) extends Condition(_id,dateDebut,dateFin,commentaire,modules)
-case class Calibration(_id:BSONObjectID=BSONObjectID.generate,dateDebut:Date,dateFin:Option[Date],commentaire:Option[String],modules:BSONObjectID,params:List[BSONObjectID]) extends Condition(_id,dateDebut,dateFin,commentaire,modules)
-case class Terrain(_id:BSONObjectID=BSONObjectID.generate,dateDebut:Date,dateFin:Option[Date],commentaire:Option[String],modules:BSONObjectID,localisation:BSONObjectID) extends Condition(_id,dateDebut,dateFin,commentaire,modules)
+case class Condition(_id:BSONObjectID,dateDebut:Date,dateFin:Option[Date],commentaire:Option[String],modules:BSONObjectID)
 
 /**
  * Object used for condition class
  */
 object Condition{
 
-  def unapply(cond: Condition): Option[(String, JsValue)] = {
-    val (prod: Product, sub) = cond match {
-      case b: Test => (b, Json.toJson(b)(testFormat))
-      case b: Calibration => (b, Json.toJson(b)(calibrationFormat))
-      case b: Terrain => (b, Json.toJson(b)(terrainFormat))
-    }
-    Some(prod.productPrefix -> sub)
-  }
-
-  def apply(`class`: String, data: JsValue): Condition = {
-    (`class` match {
-      case "Test" => Json.fromJson[Test](data)(testFormat)
-      case "Calibration" => Json.fromJson[Calibration](data)(calibrationFormat)
-      case "Terrain" => Json.fromJson[Terrain](data)(terrainFormat)
-    }).get
-  }
-
   /**
    * Value for format Condition to JSON and JSON to Condition
    */
   implicit val conditionFormat:Format[Condition]=Json.format[Condition]
 
-  /**
-   * Value for format Test to JSON and JSON to Test
-   */
-  implicit val testFormat:Format[Test]=Json.format[Test]
-
-  /**
-   * Value for format Calibration to JSON and JSON to Calibration
-   */
-  implicit val calibrationFormat:Format[Calibration]=Json.format[Calibration]
-
-  /**
-   * Value for format Terrain to JSON and JSON to Terrain
-   */
-  implicit val terrainFormat:Format[Terrain]=Json.format[Terrain]
 }
 
 /**
